d7105e86ea460bb67c7a3f322d65ab541ad148f5
be-doing <1782904648@qq.com>
2018-11-22 22:55:56 +0800

    源代码

--- 文件通讯录/文件通讯录/文件通讯录.C
+++ 文件通讯录/文件通讯录/文件通讯录.C
@@ -33,7 +33,7 @@ struct BOOK {
 	int personcount;
 }as_book;
 
-void Check_relloc() {
+void Check_malloc() {
 	if (as_book.size < as_book.personcount) {
 		return;
 	}
@@ -45,6 +45,7 @@ void Check_relloc() {
 	}
 	free(as_book.address_book);
 	as_book.address_book = temp;
+	printf("通讯录已经扩容！\n");
 }
 int book_menu() {
 	int choice = 0;
@@ -63,14 +64,14 @@ int book_menu() {
 }
 void empty_person() {//清空和初始化的相同作用。所以这里可以写成初始化
 	as_book.size = 0;
-	memset(as_book.address_book, 0x0, sizeof(as_book.address_book));
+	
 	as_book.personcount = 5;
 	as_book.address_book = (ADbook*)malloc
 	(sizeof(ADbook)*as_book.personcount);
-	printf("通讯录已经初始化！\n");
+	//printf("通讯录已经初始化！\n");
 }
 void add_person() {
-	Check_relloc();
+	Check_malloc();
 	printf("请输入姓名：");
 	scanf("%s", as_book.address_book[as_book.size].name);
 	printf("请输入性别：");
@@ -188,7 +189,40 @@ void print_person() {
 		}
 	}
 }
+//保存联系人
+void save() {
+	printf("正在保存……\n");
+	FILE* fp = fopen("../ test.txt","w");
+	if (fp==NULL) {
+		printf("文件不存在，打开失败！\n");
+		return;//结束程序
+	}
+	for (int temp = 0; temp < as_book.size; ++temp) {
+		fwrite(&as_book.address_book[temp],sizeof(ADbook), 1 ,fp);
+	}
+	fclose(fp);
+	printf("保存成功！\n");
+	printf("已经保存 %d 条数据\n",as_book.size);
+}
+//加载联系人
+void load() {
+	printf("LOADING……\n");
+	FILE* fp = fopen("../ test.txt", "r");
+	if (fp == NULL) {
+		printf("文件打开失败，加载失败\n");
+		return;//结束程序
+	}
+	ADbook temp = { 0 };
+	while (fread(&temp,sizeof(temp),1,fp)){
+		Check_malloc();
+		as_book.address_book[as_book.size] = temp;
+		++as_book.size;
 
+	}
+	fclose(fp);
+	printf("加载成功\n");
+	printf("加载 %d 条联系人！\n",as_book.size);
+}
 void sort_person() {
 	if (as_book.size == 0)
 	{
@@ -217,6 +251,8 @@ void sort_person() {
 }
 int main() {
 	int choice = 0;
+	empty_person();
+	load();
 	//以循环打印菜单提示用户输入选择
 	while (1) {
 		//开关语句卡是实现函数，增删查改，排序，清空。
@@ -224,26 +260,32 @@ int main() {
 		switch (choice) {
 		case 1://添加联系人
 			add_person();
+			save();
 			break;
 		case 2://删除联系人
 			del_person();
+			save();
 			break;
 		case 3://查找联系人
 			find_person();
 			break;
 		case 4://修改联系人
 			alt_person();
+			save();
 			break;
 		case 5://显示所有联系人
 			print_person();
 			break;
 		case 6://清空联系人
 			empty_person();
+			save();
 			break;
 		case 7://排序联系人
 			sort_person();
+			save();
 			break;
 		case 8:
+			save();
 			printf("欢迎下次使用！\n");
 			system("pause");
 			return 0;
