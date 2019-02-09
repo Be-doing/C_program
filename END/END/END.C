9f8f0fbb7816accd46deb76b1543bb1198c9c595
be-doing <1782904648@qq.com>
2019-02-09 21:37:18 +0800

    结束

--- END/END/END.C
+++ END/END/END.C
@@ -3,21 +3,24 @@
 #include<stdlib.h>
 #include<windows.h>
 void END() {
-	char end[]="到这儿C 语言的初级学习已经到了尾声。写个程序作为结尾吧。虽然有点敷衍，但是形式所逼。";
+	char end[]="到这儿C 语言的初级学习已经到了尾声。江湖路，还在继续！";
 	char* p = end;
 	int col = 0;
-	while (*p!='\0') {
+	while (*p!='\0') 
+	{
 		Sleep(300);
 		printf("%c",*p);
 		++p;
 		++col;
-		if ((col%10)==0) {
+		if ((col%10)==0) 
+		{
 			printf("\n");
 		}
 	}
 	printf("\n");
 }
-void main() {
+void main() 
+{
 	END();
 	system("pause");
 }
